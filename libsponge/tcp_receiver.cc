f55bae331fb275659f7cd79b51cf4d4f17b134fe
LuCC-queGIT_AUTHOR_EMAIL=chen1968493689@gmail.comGIT_COMMITTER_NAME=LuCC-queGIT_COMMITTER_NAME=chen1968493689@gmail.comHEAD <LuCC-que>
2022-05-26 13:06:11 -0700

    lab 2 done, cheers up!

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -15,24 +15,48 @@ void TCPReceiver::segment_received(const TCPSegment &seg) {
     TCPHeader TCPHeader = seg.header();
     WrappingInt32 seg_seqno = TCPHeader.seqno;
 
+    bool segment_before_SYN = first_segment && !TCPHeader.syn;
+
+    if (segment_before_SYN) {
+        return;
+    }
+
     if (first_segment && TCPHeader.syn) {
         first_segment = false;
         ISN = seg_seqno;
     }
     // bool not_first_seg_and_with_data = !first_segment && !data.length();
-    abs_seqno = unwrap(seg_seqno, ISN, abs_seqno) + 1;
+    uint64_t temp_abs_seqno = unwrap(seg_seqno, ISN, abs_seqno);
 
-    _reassembler.push_substring(data, abs_seqno - 1, TCPHeader.fin);
+    // the syn have to take a index
+    // the second seg cant not occupy that
+    // otherwise it is invalid
+    bool invalid_stream = (abs_seqno == 1) && (temp_abs_seqno < abs_seqno);
 
-    if (TCPHeader.fin) {
-        abs_seqno++;
+    if (invalid_stream) {
+        return;
+    }
+
+    size_t index = temp_abs_seqno > 0 ? temp_abs_seqno - 1 : temp_abs_seqno;
+    _reassembler.push_substring(data, index, TCPHeader.fin);
+
+    if (temp_abs_seqno != abs_seqno) {
+        // tcp miss!
+        return;
     }
 
     if (data.length()) {
-        abs_seqno += data.length() - 1;
+        size_t len = _reassembler.stream_out().bytes_written();
+        abs_seqno = len + 1;  // reset
+
+    } else if (temp_abs_seqno == 0) {
+        // both syn and first seg occupy a bit
+        ++abs_seqno;
     }
 
-    return;
+    if (TCPHeader.fin || _reassembler.stream_out().input_ended()) {
+        ++abs_seqno;
+    }
 }
 
 optional<WrappingInt32> TCPReceiver::ackno() const {
@@ -43,4 +67,4 @@ optional<WrappingInt32> TCPReceiver::ackno() const {
     return wrap(abs_seqno, ISN);
 }
 
-size_t TCPReceiver::window_size() const { return _capacity - _reassembler.stream_out().bytes_written(); }
+size_t TCPReceiver::window_size() const { return _capacity - _reassembler.stream_out().buffer_size(); }
